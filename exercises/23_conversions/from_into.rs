99bc9d195020a0ff803ed06afe9a4bef736870ef
sondresj <sondresj@pm.me>
2024-01-25 14:58:36 +0100

    mer

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {
+    fn from(s: &str) -> Self {
+        if s.len() == 0 {
+            return Self::default();
+        }
+        let mut parts = s.split(',');
+        let name = parts.next().unwrap();
+        if name.len() == 0 {
+            return Self::default();
+        }
+        let age = parts.next().map(|age| age.parse::<usize>().ok()).flatten();
+        if age.is_none() {
+            return Self::default();
+        }
+        Self {
+            name: name.into(),
+            age: age.unwrap(),
+        }
     }
 }
 
